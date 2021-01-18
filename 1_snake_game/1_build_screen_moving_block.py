a4978351feb91367a7576ce64b608864736bb6a6
bhanu-3b <77564522+bhanu-3b@users.noreply.github.com>
2021-01-18 17:26:41 +0530

    Update 1_build_screen_moving_block.py
    new  for your youtube watchers
    doing practice with pull request

--- 1_snake_game/1_build_screen_moving_block.py
+++ 1_snake_game/1_build_screen_moving_block.py
@@ -7,6 +7,7 @@ def draw_block():
     surface.fill((110, 110, 5))
     surface.blit(block, (block_x, block_y))
     pygame.display.flip()
+    print('selected staze for game')
 
 if __name__ == '__main__':
     pygame.init()
